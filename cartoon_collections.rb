b784f498365c0ec55962955246d95fc5c595fa3a
assane savadogo <assane.savadogo81@gmail.com>
2019-07-26 18:44:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |type| 
-  cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types.include?(types)
+  end
 end
